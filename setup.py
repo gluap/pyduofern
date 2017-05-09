f09ff0930416da1418d7fa73c513479fdbb70d3c
gluap <paul@pgoergen.de>
2017-05-09 22:51:22 +0200

    up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pyduofern',
-      version='0.111111',
+      version='0.1111111',
       description='Library for controlling Rademacher DuoFern actors using python. Requires the Rademacher'
                   'Duofern USB Stick Art.-Nr.: 70000093',
       classifiers=[
