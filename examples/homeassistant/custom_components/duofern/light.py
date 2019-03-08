7cebd0c138fb2ecf334dae5b84f49c4edb2c05a8
paulg <github@pgoergen.de>
2019-03-08 23:24:06 +0100

    Revert "working autodiscovery"
    This reverts commit 2acb114d1fafb0d31d1e99d609669bb95321d839.

--- examples/homeassistant/custom_components/duofern/light.py
+++ examples/homeassistant/custom_components/duofern/light.py
@@ -8,9 +8,6 @@ import voluptuous as vol
 # Import the device class from the component that you want to support
 from homeassistant.components.light import Light, PLATFORM_SCHEMA
 
-from . import DuofernDevice
-from .const import DOMAIN
-
 # Home Assistant depends on 3rd party packages for API specific code.
 REQUIREMENTS = ['pyduofern==0.23.5']
 
@@ -24,7 +21,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the Awesome Light platform."""
 
     # Assign configuration variables. The configuration check takes care they are
@@ -33,16 +30,22 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     stick = hass.data["duofern"]['stick']
 
     # Add devices
-    for cover in [dev for dev in stick.config['devices'] if dev['id'].startswith('46')]:
-        if cover['id'] not in hass.data[DOMAIN]['unique_ids']:
-            hass.data[DOMAIN]['unique_ids'].add(cover['id'])
-            add_entities([DuofernLight(cover['id'], cover['name'], stick, hass)])
+    add_devices(DuofernLight(device['id'], device['name'], stick) for device in stick.config['devices'] if
+                device['id'].startswith('46'))
 
 
-class DuofernLight(DuofernDevice, Light):
-    def __init__(self, *args, **kwargs):
+class DuofernLight(Light):
+    def __init__(self, id, desc, stick):
         """Initialize the shutter."""
-        super().__init__(*args, **kwargs)
+        self._id = id
+        self._name = desc
+        self._state = None
+        self._brightness = None
+        self._stick = stick
+
+    @property
+    def name(self):
+        return self._name
 
     @property
     def is_on(self):
@@ -61,3 +64,6 @@ class DuofernLight(DuofernDevice, Light):
         self._stick.command(self._id, "off")
         # this is a hotfix because currently the state is not correctly detected from duofern
         self._stick.duofern_parser.modules['by_code'][self._id]['state'] = 0
+
+    def update(self):
+        pass
